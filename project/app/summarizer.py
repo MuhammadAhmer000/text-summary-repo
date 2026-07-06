ff35d15724be4b3767fc5fe3cccec1f93cb44faa
Muhammad Ahmer <muahmer534@gmail.com>
2026-07-06 12:57:42 +0400

    add text summarization with newspaper3k

---
+++ project/app/summarizer.py
@@ -0,0 +1,20 @@
+# project/app/summarizer.py
+
+
+import nltk
+from newspaper import Article
+
+
+def generate_summary(url: str) -> str:
+    article = Article(url)
+    article.download()
+    article.parse()
+
+    try:
+        nltk.data.find("tokenizers/punkt_tab")
+    except LookupError:
+        nltk.download("punkt_tab")
+    finally:
+        article.nlp()
+
+    return article.summary
