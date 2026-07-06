f5d36dca0eb67512afcc3dc4c3f8e110a256e70f
Muhammad Ahmer <muahmer534@gmail.com>
2026-07-06 12:17:40 +0400

    development of app

---
+++ project/app/config.py
@@ -0,0 +1,16 @@
+import logging
+from functools import lru_cache
+from pydantic_settings import BaseSettings
+from pydantic import AnyUrl
+
+log = logging.getLogger("uvicorn")
+
+class Settings(BaseSettings):
+    environment: str = "dev"
+    testing: bool = bool(0)
+    database_url: AnyUrl = None
+
+@lru_cache()
+def get_settings() -> BaseSettings:
+    log.info("Loading config settings from environment")
+    return Settings()
